d1fad50f416b603ae4323b07755d84516edba008
Enegadi Ismail <ismail0enegadi@gmail.com>
2024-03-04 21:58:25 +0100

    explore the feature for queries in JpaRepository interface

--- src/main/java/dev/enegadi/jeetp2/JeeTp2Application.java
+++ src/main/java/dev/enegadi/jeetp2/JeeTp2Application.java
@@ -22,8 +22,8 @@ public class JeeTp2Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// add some patients to database
-		patientRepository.save(new Patient(1, "Ismail Enegadi", new Date(), 1000, false ));
-		patientRepository.save(new Patient(2, "Idriss Merzak", new Date(), 1000, true ));
+		// patientRepository.save(new Patient(null, "Ismail Enegadi", new Date(), 1000, false ));
+		// patientRepository.save(new Patient(null, "Idriss Merzak", new Date(), 1000, true ));
 		// get all the patients from the db
 		System.out.println("-----------Those are all the Patient in the db---------------");
 		List<Patient> patients = patientRepository.findAll();
@@ -32,9 +32,25 @@ public class JeeTp2Application implements CommandLineRunner {
 		}
 		// trying to get a Patient by Id
 		System.out.println("---------------This is a Patient with an id equal to 2-----------------");
-		Patient patientById = patientRepository.findById(2L).get();
-		System.out.println(patientById.toString());
-
-
+        Patient patientById = patientRepository.findById(2L).get();
+        System.out.println(patientById.toString());
+        // trying to get a Patient by Name using PatientRepository
+        System.out.println("---------------This is a Patient with name equal to 'Ismail Enegadi'-----------------");
+        List<Patient> patientList =  patientRepository.findPatientByName("Ismail Enegadi");
+        patientList.forEach(patient -> {
+            System.out.println(patient.toString());
+        });
+        // trying to get a Patient by Name using PatientRepository using my query
+        System.out.println("---------------This is a Patient with name equal to 'Enegadi' using my function-----------------");
+        List<Patient> patientList1 =  patientRepository.findPatientByQuery("Ismail Enegadi");
+        patientList1.forEach(patient -> {
+            System.out.println(patient.toString());
+        });
+        // trying ot get all the patients who have a score grather than 500
+        System.out.println("---------------------- Those are the patient who have score greater than 500 --------------------");
+        List<Patient> patientsByScore  = patientRepository.getPatientByScoreGreaterThan(500);
+        patientsByScore.forEach(patient -> {
+            System.out.println(patient.toString());
+        });
 	}
 }
--- src/main/java/dev/enegadi/jeetp2/Reposotories/PatientRepository.java
+++ src/main/java/dev/enegadi/jeetp2/Reposotories/PatientRepository.java
@@ -2,6 +2,17 @@ package dev.enegadi.jeetp2.Reposotories;
 
 import dev.enegadi.jeetp2.Entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient,  Long> {
+
+    List<Patient> findPatientByName(String name);
+
+    @Query("select p from Patient p where p.name like :x")
+    List<Patient> findPatientByQuery(@Param("x") String nameQuery);
+
+    List<Patient> getPatientByScoreGreaterThan(int score);
 }
